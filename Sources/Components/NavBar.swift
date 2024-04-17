3ba5523750cec5abcfe5b1446c1e77a878d9997d
Paul Hudson <paul.hudson@gmail.com>
2024-04-17 15:37:55 +0100

    Updated the logo to work better on mobile devices.

--- Sources/Components/NavBar.swift
+++ Sources/Components/NavBar.swift
@@ -11,7 +11,7 @@ import Ignite
 /// An example navigation bar, demonstrating how to create reusable components.
 struct NavBar: Component {
     func body(context: PublishingContext) -> [any PageElement] {
-        NavigationBar(logo: Image("/images/logo.svg", description: "ExampleSite logo").frame(width: "300px", height: "100%")) {
+        NavigationBar(logo: Image("/images/logo.svg", description: "ExampleSite logo").frame(width: "min(60vw, 300px)", height: "100%")) {
             Link("Table of Contents", target: "/")
 
             Link("Ignite on GitHub", target: "https://github.com/twostraws/Ignite")
