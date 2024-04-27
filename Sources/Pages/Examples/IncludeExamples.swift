8c67c41601dd6718b54462f8507ec49975dae882
Paul Hudson <paul.hudson@gmail.com>
2024-04-27 14:09:51 +0100

    Added demonstration of resource loading.

--- Sources/Pages/Examples/IncludeExamples.swift
+++ Sources/Pages/Examples/IncludeExamples.swift
@@ -26,8 +26,42 @@ struct IncludeExamples: StaticPage {
 
         Include("important.html")
 
+        Text("Loading arbitrary data")
+            .font(.title2)
+            .margin(.top, .extraLarge)
+
+        Text(markdown: "If you want to load arbitrary resources for parsing in your Swift code, you should create a folder called **Resources** then you have three options.")
+
+        Text(markdown: "First, `context.url(forResource:)` tells you the full path to a file in Resources.")
+
+        Text(markdown: "Second, `context.data(forResource:)` locates a file in Resources and loads it into a `Data` instance. This uses `context.url(forResource:)` internally.")
+
+        Text(markdown: "And third, `context.decode(resource:as:)` locates a JSON file, loads it into a `Data` instance, then decodes it to a `Decodable` type of your choosing. This uses `context.data(forResource:)` internally, which in turn uses `context.url(forResource:)`.")
+
+        Text("For example, this project contains some JSON in Resources/quotes.json, so we can display its contents using the below:")
+
+        CodeBlock(language: "swift", """
+        if let quotes = context.decode(resource: "quotes.json", as: [String].self) {
+            for quote in quotes {
+                Quote {
+                    quote
+                }
+            }
+        }
+        """)
+
+        if let quotes = context.decode(resource: "quotes.json", as: [String].self) {
+            for quote in quotes {
+                Quote {
+                    quote
+                }
+            }
+        }
+
+
         Alert {
             Text(markdown: "**Tip:** This page uses a custom theme, which is dark")
         }
+        .role(.primary)
     }
 }
