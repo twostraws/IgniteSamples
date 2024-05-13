bbb10ad36d33dcd9d35e9e258fbd563cf1e3ed80
Philipp <pd95@users.noreply.github.com>
2024-05-13 19:41:25 +0200

    Adding `Link`s to all chapters as `DropDown`s.

--- Sources/Components/NavBar.swift
+++ Sources/Components/NavBar.swift
@@ -14,6 +14,32 @@ struct NavBar: Component {
         NavigationBar(logo: Image("/images/logo.svg", description: "ExampleSite logo").frame(width: "min(60vw, 300px)", height: "100%")) {
             Link("Table of Contents", target: "/")
 
+            Dropdown("Key concepts") {
+                Link("Grid Layout", target: GridExamples())
+                Link("Navigation", target: NavigationExamples())
+                Link("Content", target: ContentExamples())
+                Link("Text", target: TextExamples())
+                Link("Styling", target: StylingExamples())
+            }
+
+            Dropdown("Examples") {
+                Link("Accordions", target: AccordionExamples())
+                Link("Alerts", target: AlertExamples())
+                Link("Badges", target: BadgeExamples())
+                Link("Buttons", target: ButtonExamples())
+                Link("Cards", target: CardExamples())
+                Link("Carousels", target: CarouselExamples())
+                Link("Code", target: CodeExamples())
+                Link("Dropdowns", target: DropdownExamples())
+                Link("Embeds", target: EmbedExamples())
+                Link("Images", target: ImageExamples())
+                Link("Includes", target: IncludeExamples())
+                Link("Links", target: LinkExamples())
+                Link("Lists", target: ListExamples())
+                Link("Quotes", target: QuoteExamples())
+                Link("Tables", target: TableExamples())
+            }
+
             Link("Ignite on GitHub", target: "https://github.com/twostraws/Ignite")
 
             Dropdown("@twostraws") {
