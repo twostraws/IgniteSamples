033093e1c39f6f0ce464b6e29c583439d8abbf46
Philipp <pd95@users.noreply.github.com>
2024-05-14 19:59:04 +0200

    Remove "Table of Contents" and "@twostraws"

--- Sources/Components/NavBar.swift
+++ Sources/Components/NavBar.swift
@@ -12,7 +12,6 @@ import Ignite
 struct NavBar: Component {
     func body(context: PublishingContext) -> [any PageElement] {
         NavigationBar(logo: Image("/images/logo.svg", description: "ExampleSite logo").frame(width: "min(60vw, 300px)", height: "100%")) {
-            Link("Table of Contents", target: "/")
 
             Dropdown("Key concepts") {
                 Link("Grid Layout", target: GridExamples())
@@ -41,12 +40,6 @@ struct NavBar: Component {
             }
 
             Link("Ignite on GitHub", target: "https://github.com/twostraws/Ignite")
-
-            Dropdown("@twostraws") {
-                Link("Mastodon", target: "https://mastodon.social/@twostraws")
-                Link("Twitter", target: "https://twitter.com/twostraws")
-                Link("YouTube", target: "https://youtube.com/@twostraws")
-            }
         }
         .navigationItemAlignment(.trailing)
         .navigationBarStyle(.dark)
